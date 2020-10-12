b7f44b5b3188a8080353d1eb5a858eafd09dc147
Marcos Botene <marcosjbotene@gmail.com>
2020-10-12 20:36:49 -0300

    Add Routes.

--- happy-web/src/App.tsx
+++ happy-web/src/App.tsx
@@ -1,31 +1,9 @@
 import React from 'react';
-import { FiArrowRight } from 'react-icons/fi';
+
+import Routes from './routes';
 
 import './styles/global.css';
-import './styles/pages/landing.css';
 
-import logoImg from './images/logo.svg';
-
-export default function LandingPage() {
-  return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy" />
-
-        <main>
-          <h1>Leve a felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-        </main>
-
-        <div className="location">
-          <strong>Brusque</strong>
-          <span>Santa Catarina</span>
-        </div>
-
-        <a href="" className="enter-app">
-          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-        </a>
-      </div>
-    </div>
-  );
+export default function App() {
+  return <Routes />;
 }
