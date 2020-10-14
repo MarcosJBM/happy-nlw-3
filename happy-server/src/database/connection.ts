822f0db81c91015cbc3260b1e2ede18b104248e5
Marcos Botene <marcosjbotene@gmail.com>
2020-10-14 11:45:12 -0300

    Configurando o Banco de Dados.

---
+++ happy-server/src/database/connection.ts
@@ -0,0 +1,5 @@
+import { createConnection } from 'typeorm';
+
+createConnection();
+// Conexão com o Banco de Dados.
+// ormconfig.json → informações do Banco de Dados.
--- happy-server/src/server.ts
+++ happy-server/src/server.ts
@@ -1,5 +1,12 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
+
+app.use(express.json());
+
+// :Parametrô
+app.get('/users', (request, response) => {});
 
 app.listen(3333);
