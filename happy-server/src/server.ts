a0a9d7656b338111a0b404215b4babf887f3e473
Marcos Botene <marcosjbotene@gmail.com>
2020-10-16 17:02:19 -0300

    Added creation of an orphanage.

--- happy-server/src/server.ts
+++ happy-server/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
 
 import './database/connection';
 
@@ -7,6 +9,32 @@ const app = express();
 app.use(express.json());
 
 // :Parametrô
-app.get('/users', (request, response) => {});
+app.post('/orphanages', async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  await orphanagesRepository.save(orphanage);
+
+  return response.json({ message: 'Orfanato Criado' });
+});
 
 app.listen(3333);
