561d02780049fb0280665174348fb9f0b52ab2e5
Marcos Botene <marcosjbotene@gmail.com>
2020-10-16 18:29:10 -0300

    Orphanage Listing Added.

--- happy-server/src/routes.ts
+++ happy-server/src/routes.ts
@@ -4,6 +4,7 @@ import OrphanagesController from './controllers/OrphanagesController';
 const routes = Router();
 
 // :Parametrô
+routes.get('/orphanages', OrphanagesController.index);
 routes.post('/orphanages', OrphanagesController.create);
 
 export default routes;
