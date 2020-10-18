5f19d3b5a65c6cd2f59601bc154d0f69c19ebe4f
Marcos Botene <marcosjbotene@gmail.com>
2020-10-18 20:50:43 -0300

    Added Routes in Mobile.

---
+++ happy-mobile/src/pages/OrphanagesDetails/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function OrphanagesDetails() {
+  return (
+    <View>
+      <Text>Opá</Text>
+    </View>
+  );
+}
